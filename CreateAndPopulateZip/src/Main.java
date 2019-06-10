0a9d7704731cb549d46b19c4502d7558c7de271b
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-06-10 18:04:10 +0200

    output by Fils.newBufferedWriter() and Files.write()

--- CreateAndPopulateZip/src/Main.java
+++ CreateAndPopulateZip/src/Main.java
@@ -1,7 +1,10 @@
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.charset.Charset;
 import java.nio.file.*;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,6 +20,9 @@ public class Main {
 
         try(FileSystem zipFs = openZip(Paths.get("myData.zip"))){
             copyToZip(zipFs);
+            writeToFileZip1(zipFs,data);
+            writeToFileZip2(zipFs,data);
+
         }catch (Exception e){
             System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
         }
@@ -34,17 +40,36 @@ public class Main {
     private static void copyToZip(FileSystem zipFs) throws IOException{
         Path sourceFile = Paths.get("file1.txt");
         //Path sourceFile = FileSystems.getDefault().getPath("file1.txt");
-        Path destinationFile = zipFs.getPath("file1Copied.txt");
+        Path destinationFile = zipFs.getPath("/file1Copied.txt");
 
         Files.copy(sourceFile,destinationFile,StandardCopyOption.REPLACE_EXISTING);
 
     }
 
     private static void writeToFileZip1(FileSystem zipFs,String[] data)throws IOException{
+        try(BufferedWriter writer = Files.newBufferedWriter(zipFs.getPath("/newFile1.txt"))){
+            for(String str: data){
+                writer.write(str);
+                writer.newLine();
+            }
+        }catch (Exception e){
+            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+        }
 
     }
 
     private static void writeToFileZip2(FileSystem zipFs,String[] data) throws IOException{
+       /*
+       try(BufferedWriter writer = Files.newBufferedWriter(Paths.get("newFile2.txt"));){
+            for(String str: data){
+                writer.write(str);
+                writer.newLine();
+            }
+        }catch (Exception e){
+            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+        }
 
+        */
+       Files.write(Paths.get("newFile2Test.txt"), Arrays.asList(data), Charset.defaultCharset(),StandardOpenOption.CREATE);
     }
 }

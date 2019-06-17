25c45beba27c769a2b0a11e7729485efc7da1e05
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-06-17 13:16:09 +0200

    parameterised log

--- MakingLogCalls/src/Main.java
+++ MakingLogCalls/src/Main.java
@@ -7,6 +7,7 @@ public class Main {
     static LogManager logManager = LogManager.getLogManager();
     static Logger logger = logManager.getLogger(Logger.GLOBAL_LOGGER_NAME);
     public static void main(String[] args) {
+        logger.setLevel(Level.FINER);
         /*
         logger.log(Level.INFO,"My first log message");
         logger.log(Level.INFO,"Another message");
@@ -21,15 +22,23 @@ public class Main {
         //logger.log(Level.FINEST,"Special explanation");
         //logger.severe("severe level");
 
-        //doWork();
-        logger.log(Level.INFO,"{0} is my favorite","Java");
-        logger.log(Level.INFO,"{0} is {1} days from {2}",new Object[]{"Wed",2,"Fri"});
+        doWork();
+        //logger.log(Level.INFO,"{0} is my favorite","Java");
+        //logger.log(Level.INFO,"{0} is {1} days from {2}",new Object[]{"Wed",2,"Fri"});
+        parameterisedLog("Jim","Jones");
     }
 
     private static void doWork(){
-        logger.setLevel(Level.ALL);
-        logger.entering("com.jwhh.support.Other","doWork");
-        logger.logp(Level.WARNING,"com.jwhh.support.Other","doWork","Empty Function");
-        logger.exiting("com.jwhh.support.Other","doWork");
+
+        logger.entering("com.jwhh.support.Other","doWork()");
+        logger.logp(Level.WARNING,"com.jwhh.support.Other","doWork()","Empty Function");
+        logger.exiting("com.jwhh.support.Other","doWork()");
+    }
+
+    private static void parameterisedLog(String left, String right){
+        logger.entering("com.jwhh.support.Other","parameterisedLog()",new Object[]{left,right});
+        String result = "< "+left+right+" >";
+        logger.logp(Level.INFO,"this class","paramterisedLog","{0},{1} is name",new Object[]{left,right});
+        logger.exiting("com.jwhh.support.Other","parameterisedLog()",result);
     }
 }

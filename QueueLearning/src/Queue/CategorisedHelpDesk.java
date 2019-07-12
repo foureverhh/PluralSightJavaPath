da7b462ee0180c54ac24ae1708c742f4f19ce7f7
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-07-12 14:32:16 +0800

    Make code neat by adding processEnquiry()

--- QueueLearning/src/Queue/CategorisedHelpDesk.java
+++ QueueLearning/src/Queue/CategorisedHelpDesk.java
@@ -24,27 +24,35 @@ public class CategorisedHelpDesk {
     public void processEnquiry(Predicate predicate, String message){
         Enquiry enquiry = enquiries.peek();
         if(enquiry != null && predicate.test(enquiry)){
-
+            enquiries.remove();
+            System.out.println(message);
+        }else {
+            System.out.println("No work to do, let's have some coffee!");
         }
     }
 
     public void processPrinterEnquiry(){
        Predicate<Enquiry> predicate = enquiry -> enquiry.getCategory() == PRINTER;
-       Enquiry enquiry = enquiries.peek();
+       final String MESSAGE = "Is it out of paper?" ;
+       //Enquiry enquiry = enquiries.peek();
        //if(enquiry != null && enquiry.getCategory() == PRINTER)
-        if(enquiry !=null && predicate.test(enquiry))
+       /* if(enquiry !=null && predicate.test(enquiry))
        {
            enquiries.remove();
            enquiry.getCustomer().reply("Is it out of Paper?");
        }else {
            System.out.println("No work to do, let's have some coffee!");
        }
+        */
+       processEnquiry(predicate,MESSAGE);
     }
 
     public void processGeneralEnquiry(){
         Predicate<Enquiry> predicate = enquiry -> enquiry.getCategory() != PRINTER;
         Enquiry enquiry = enquiries.peek();
+        processEnquiry(predicate,MESSAGE);
         //if(enquiry != null && enquiry.getCategory() != PRINTER)
+        /*
         if(enquiry !=null && predicate.test(enquiry))
         {
             enquiries.remove();
@@ -52,6 +60,7 @@ public class CategorisedHelpDesk {
         }else {
             System.out.println("No work to do, let's have some coffee!");
         }
+        */
     }
 
 }

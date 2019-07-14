06213958b4a7ea939c5322de082d8c9eeb8103da
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-07-14 15:12:10 +0800

    Deque to settle calculator

--- CalculatorWithDeque/src/Calculator.java
+++ CalculatorWithDeque/src/Calculator.java
@@ -1,3 +1,7 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
 public class Calculator {
     /*
     1
@@ -9,6 +13,35 @@ public class Calculator {
     4
     */
     public int evaluate(String input){
-        return 0;
+        Deque<String> stack = new ArrayDeque<>();
+        String[] tokens = input.split("");
+
+            //System.out.println(str);
+            stack.addAll(Arrays.asList(tokens));
+
+        int result = 0;
+        while(stack.size()>1){
+            int left = Integer.parseInt(stack.pop());
+            System.out.println("number is: "+left);
+            String operator = stack.pop();
+            int right = Integer.parseInt(stack.pop());
+
+            switch (operator){
+                case "+":
+                    result = left + right;
+                    break;
+                case "-":
+                    result = left - right;
+                    break;
+                default:
+                    break;
+            }
+            System.out.println("Result: "+result);
+            stack.push(String.valueOf(result));
+        }
+
+        return Integer.parseInt(stack.pop());
     }
+
+
 }
--- CalculatorWithDeque/src/Main.java
+++ CalculatorWithDeque/src/Main.java
@@ -1,6 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
 
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String input = getInput("number.txt");
+        Calculator calculator = new Calculator();
+        int result = calculator.evaluate(input);
+        System.out.println("Result is: "+result);
+    }
+
+    public static String getInput(String file){
+        StringBuilder input = new StringBuilder();
+        try(BufferedReader reader = Files.newBufferedReader(Paths.get(file))){
+            String str = null;
+            while((str = reader.readLine())!= null){
+                //System.out.println(str);
+                input.append(str);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return input.toString();
     }
 }

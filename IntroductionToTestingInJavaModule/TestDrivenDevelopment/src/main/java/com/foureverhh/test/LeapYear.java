ae09d46e84c87ceebdce957fef36035d39c1fbb8
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-07-31 17:26:16 +0200

    make arguments final

--- IntroductionToTestingInJavaModule/TestDrivenDevelopment/src/main/java/com/foureverhh/test/LeapYear.java
+++ IntroductionToTestingInJavaModule/TestDrivenDevelopment/src/main/java/com/foureverhh/test/LeapYear.java
@@ -2,12 +2,12 @@ package com.foureverhh.test;
 
 public class LeapYear {
 
-    public static boolean isLeapYear(int year) {
+    public static boolean isLeapYear(final int year) {
 
         return isDivisible(year,4) && (!isDivisible(year,100)|| isDivisible(year,400));
     }
 
-    private static boolean isDivisible(int year,int denominator) {
+    private static boolean isDivisible(final int year,final int denominator) {
         return year % denominator == 0;
     }
 

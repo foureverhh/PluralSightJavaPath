ae1640023fe553f925e008d9b87f3375091b7688
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-07-31 21:39:46 +0200

    split seems work, but not anyway case

--- IntroductionToTestingInJavaModule/TestDrivenDevelopment/src/main/java/com/foureverhh/test/WordWrap.java
+++ IntroductionToTestingInJavaModule/TestDrivenDevelopment/src/main/java/com/foureverhh/test/WordWrap.java
@@ -1,4 +1,23 @@
 package com.foureverhh.test;
 
 public class WordWrap {
+    public static String wrap(final String inputLine,final int lineLength) {
+        final StringBuilder accumulator = new StringBuilder();
+        final int length = inputLine.length();
+        accumulator.append(inputLine,0,Math.min(length,lineLength));
+
+        int split = lineLength;
+        if(length > split){
+            accumulator.append("\n");
+            accumulator.append(inputLine, split, split + lineLength);
+        }
+        split += lineLength;
+        if(length > split){
+            accumulator.append("\n");
+            accumulator.append(inputLine, split, length);
+        }
+
+        return accumulator.toString();
+    }
 }
+
--- IntroductionToTestingInJavaModule/TestDrivenDevelopment/src/test/java/WordWrapTest.java
+++ IntroductionToTestingInJavaModule/TestDrivenDevelopment/src/test/java/WordWrapTest.java
@@ -1,8 +1,28 @@
 import com.foureverhh.test.LeapYear;
+import com.foureverhh.test.WordWrap;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class WordWrapTest {
+    public static final int LINE_LENGTH = 5;
+    //A method that breaks words on specified space with new line. like a word processor would.
 
+    @Test
+    public void lineShouldWrapIfOverLineLength(){
+        String result = WordWrap.wrap("The Sleepy",LINE_LENGTH);
+        Assert.assertEquals("The S\nleepy",result);
+    }
+
+    @Test
+    public void shortLinesShouldNotWrap(){
+        String result = WordWrap.wrap("The",LINE_LENGTH);
+        Assert.assertEquals("The",result);
+    }
+
+    @Test
+    public void longerLineShouldWrapTwice(){
+        String result = WordWrap.wrap("The Sleepy Brow",LINE_LENGTH);
+        Assert.assertEquals("The S\nleepy\n Brow",result);
+    }
 }
 

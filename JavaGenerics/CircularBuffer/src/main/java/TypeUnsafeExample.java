ded0cbe2228d412c54e70ec5084e527922598035
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-08-16 16:11:14 +0200

    As no generics involved, have to manually change instance type

--- JavaGenerics/CircularBuffer/src/main/java/TypeUnsafeExample.java
+++ JavaGenerics/CircularBuffer/src/main/java/TypeUnsafeExample.java
@@ -1,21 +1,22 @@
 public class TypeUnsafeExample {
     public static void main(String[] args) {
-        CircularBuffer buffer = new CircularBuffer(10);
+        StringCircularBuffer buffer = new StringCircularBuffer(10);
 
         buffer.offer("a");
         buffer.offer("bc");
         buffer.offer("d");
 
-        buffer.offer(1);
+        //buffer.offer(1);
 
         String value = concatenate(buffer);
         System.out.println(value);
     }
 
-    public static String concatenate(CircularBuffer buffer){
+    public static String concatenate(StringCircularBuffer buffer){
         StringBuffer result = new StringBuffer();
         String value;
-        while((value=(String)buffer.poll())!=null){
+        //while((value=(String)buffer.poll())!=null){
+        while((value = buffer.poll())!=null){
             result.append(value);
         }
         return result.toString();

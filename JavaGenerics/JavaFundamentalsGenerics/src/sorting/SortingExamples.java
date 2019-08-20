941b0e307dc8e3db7d6587034efdc2a975131a84
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-08-20 11:07:42 +0200

    Use generics as parameter in ResverseComparator class

--- JavaGenerics/JavaFundamentalsGenerics/src/sorting/SortingExamples.java
+++ JavaGenerics/JavaFundamentalsGenerics/src/sorting/SortingExamples.java
@@ -4,10 +4,21 @@ import model.Person;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class SortingExamples {
     public static void main(String[] args) {
+        /*
+        //Sort descending
+         final Comparator<Person> descendingAge = new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return -Integer.compare(o1.getAge(),o2.getAge());
+            }
+        };
+        */
+
         Person donDraper = new Person("Don Draper",89);
         Person peggyOlson = new Person("Peggy Olson", 65);
         Person bertCooper = new Person("Bert Cooper", 100);
@@ -19,10 +30,17 @@ public class SortingExamples {
 
         System.out.println(madMen);
         //Sort ascending
-        Collections.sort(madMen,new AgeComparator());
-        //Sort descending
-
+        Comparator<Person> ascendingComparator = new AgeComparator();
+        Collections.sort(madMen,ascendingComparator);
         System.out.println();
+        System.out.println("Ascending order");
+        System.out.println(madMen);
+        //Sort descending
+        ReverseComparator<Person> descendingComparator = new ReverseComparator<>(ascendingComparator);
+        //Collections.sort(madMen,descendingAge);
+        Collections.sort(madMen,descendingComparator);
+        System.out.println();
+        System.out.println("Descending order");
         System.out.println(madMen);
     }
 

7f3012216dcc42751ae266aca8426cfac0df38a0
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-10-07 19:13:30 +0200

    Use Object[] and list to store record

---
+++ Java300/TestORM/src/main/java/demo01/Demo01.java
@@ -0,0 +1,49 @@
+package demo01;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+* Use Object[] to store a record
+* */
+public class Demo01 {
+    public static void main(String[] args) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        List<Object[]> objs = new ArrayList<Object[]>();
+
+
+        try {
+            conn = JDBCUtils.getMysqlConnection();
+            ps = conn.prepareStatement("SELECT empname,salary,age FROM emp WHERE id>=? ");
+            ps.setObject(1,1);
+            //ps.setObject(2,4);
+            rs =ps.executeQuery();
+            //Object[] obj = new Object[3];
+            while (rs.next()){
+                Object[] obj = new Object[3];
+                //System.out.println(rs.getString(1)+" "+rs.getString(2)+" "+rs.getString(3));
+                obj[0] = rs.getString(1);
+                obj[1] = rs.getObject(2);
+                obj[2] = rs.getObject(3);
+                objs.add(obj);
+                //System.out.println(obj.length);
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.close(conn,ps,rs);
+        }
+        //System.out.println(""+obj[0]+obj[1]+obj[2]);
+        System.out.println(""+objs.get(0)[0]+objs.get(0)[1]+objs.get(0)[2]);
+        System.out.println(""+objs.get(1)[0]+objs.get(1)[1]+objs.get(1)[2]);
+
+    }
+}
---
+++ Java300/TestORM/src/main/java/demo01/JDBCUtils.java
@@ -0,0 +1,109 @@
+package demo01;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+public class JDBCUtils {
+    public static Properties pros = null;
+    static {
+        try {
+        pros = new Properties();
+        pros.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("db.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getMysqlConnection(){
+        Connection conn = null;
+        try {
+            Class.forName(pros.getProperty("mysqlDriver"));
+            conn = DriverManager.getConnection(pros.getProperty("mysqlURL")
+                    ,pros.getProperty("mysqlUName")
+                    ,pros.getProperty("mysqlPwd"));
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static Connection getMysqlConnection1(){
+        Connection conn = null;
+        try {
+            //Class.forName(pros.getProperty("mysqlDriver"));
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sorm?useSSL=false&serverTimezone=UTC"
+                    ,"root"
+                    ,"!!8603122692167");
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void close(Connection conn, Statement ps, ResultSet rs){
+
+        try {
+            if(null!=rs) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+                e.printStackTrace();
+        }
+
+        try {
+                if(null!=ps) {
+                    ps.close();
+                }
+        } catch (SQLException e) {
+                e.printStackTrace();
+        }
+
+
+        try {
+                if(null!=conn) {
+                    conn.close();
+                }
+        } catch (SQLException e) {
+                e.printStackTrace();
+        }
+
+    }
+
+    public static void close(Connection conn, Statement ps){
+        try {
+            if(null!=ps) {
+                ps.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            if(null!=conn) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close(Connection conn){
+        try {
+            if(null!=conn) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}

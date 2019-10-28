1e22142262db2e8cdaca254545c67a84909ce8a9
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-10-28 23:19:16 +0100

    webapp must under src/main as facets must get the paht

--- PluralSight/JavaSE/JavaWebFundamentals/src/main/java/servletDemo/SimpleServlet.java
+++ PluralSight/JavaSE/JavaWebFundamentals/src/main/java/servletDemo/SimpleServlet.java
@@ -11,6 +11,21 @@ import java.io.IOException;
 public class SimpleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Hello World");
+        String name = req.getParameter("name");
+        if(name!=null) {
+            resp.getWriter().printf("Hello World %s", name);
+        }else {
+            resp.getWriter().write("Please enter a name");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        if(name!=null) {
+            resp.getWriter().printf("Hello World %s", name);
+        }else {
+            resp.getWriter().write("Please enter a name");
+        }
     }
 }
---
+++ PluralSight/JavaSE/JavaWebFundamentals/src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+      <form action="home" method="post">
+        <p>
+          <label for="userName">Name:</label>
+          <input id="userName" type="text" name="name"/>
+        </p>
+        <p>
+          <input type="submit" value="Enter name"/>
+        </p>
+      </form>
+  </body>
+</html>

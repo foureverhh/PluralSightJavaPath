a04f4f37e6de5e57e7713f9a5eba24a4438b96e8
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-10-29 15:39:11 +0100

    tomcat works

--- PluralSight/JavaSE/JavaWebFundamentals/src/main/java/servletDemo/SimpleServlet.java
+++ PluralSight/JavaSE/JavaWebFundamentals/src/main/java/servletDemo/SimpleServlet.java
@@ -1,19 +1,35 @@
 package servletDemo;
 
+
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/home","*.do"})
+@WebServlet(urlPatterns = {"/home","*.do"}, name = "JavaWebFundamentals", initParams = {@WebInitParam(name="ProductName",value="Welcome Application")})
 public class SimpleServlet extends HttpServlet {
+    String appName = "My Application";
+    @Override
+    public void init() throws ServletException {
+        appName = getServletContext().getInitParameter("ProductName");
+
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("name");
         if(name!=null) {
-            resp.getWriter().printf("Hello World %s", name);
+            resp.setContentType("text/xml");
+            //resp.getWriter().printf("Hello World %s", name);
+            //resp.getWriter().write("<h1>This is "+name+"</h1>");
+            resp.getWriter().printf("<application>" +
+                    "<name>Hello %s</name>" +
+                    "<product>%s</product>" +
+                    "</application>",name,appName);
         }else {
             resp.getWriter().write("Please enter a name");
         }
@@ -22,10 +38,11 @@ public class SimpleServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("name");
-        if(name!=null) {
+        if(name!=null && name!="") {
             resp.getWriter().printf("Hello World %s", name);
         }else {
-            resp.getWriter().write("Please enter a name");
+            //resp.getWriter().write("Please enter a name");
+            resp.sendRedirect("index.jsp");
         }
     }
 }
--- PluralSight/JavaSE/JavaWebFundamentals/src/main/webapp/index.jsp
+++ PluralSight/JavaSE/JavaWebFundamentals/src/main/webapp/index.jsp
@@ -1,17 +1,40 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title></title>
+    <title>My App</title>
+  <%--  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="metro-bootstrap-master/css/metro-bootstrap.css" type="text/css">--%>
   </head>
   <body>
-      <form action="home" method="post">
-        <p>
-          <label for="userName">Name:</label>
-          <input id="userName" type="text" name="name"/>
-        </p>
-        <p>
-          <input type="submit" value="Enter name"/>
-        </p>
-      </form>
+<%--  <header class="clear-fix">
+    <div class="navbar nav-default" role="navigation">
+      <button type="button" class="navbar-toggle" data-toggle="collaps" ></button>
+    </div>
+
+  </header>
+  <section class="main container-fluid">
+    <div class="container">
+      <h1>Home</h1>
+      <div class="row-fluid">
+        <div class="col-md-3">Sidebar</div>
+        <div class="col-md-9">
+          <tabset>
+            <tab heading="Searching">--%>
+              <form action="home" method="post">
+                <p>
+                  <label for="userName">Name:</label>
+                  <input id="userName" type="text" name="name"/>
+                </p>
+                <p>
+                  <input type="submit" value="Enter name"/>
+                </p>
+              </form>
+          <%--  </tab>
+          </tabset>
+        </div>
+      </div>
+    </div>
+  </section>
+  <script src="bootstrap/js/bootstrap.js"></script>--%>
   </body>
 </html>

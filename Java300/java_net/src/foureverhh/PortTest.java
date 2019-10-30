fa05ca43c10d4d2128d26013348130c7a0aefd1a
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-10-30 11:19:49 +0100

    Test URL protocol host port file path query ref

--- Java300/java_net/src/foureverhh/PortTest.java
+++ Java300/java_net/src/foureverhh/PortTest.java
@@ -1,4 +1,16 @@
 package foureverhh;
 
+import java.net.InetSocketAddress;
+
 public class PortTest {
+    public static void main(String[] args) {
+        InetSocketAddress socketAddress1 = new InetSocketAddress("163.171.141.114",8080);
+        InetSocketAddress socketAddress2 = new InetSocketAddress("localhost",9000);
+        System.out.println(socketAddress1.getAddress());
+        System.out.println(socketAddress2.getHostName());
+        System.out.println(socketAddress1.getPort());
+        System.out.println(socketAddress2.getPort());
+    }
+
+
 }

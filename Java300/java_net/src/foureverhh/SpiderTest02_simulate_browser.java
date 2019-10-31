c332d96e632c10b41100b590ef82b34e71411ead
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-10-31 09:32:30 +0100

    Java web crawler 伪装成浏览器，User-Agent

---
+++ Java300/java_net/src/foureverhh/SpiderTest02_simulate_browser.java
@@ -0,0 +1,34 @@
+package foureverhh;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
+import java.net.URL;
+
+public class SpiderTest02_simulate_browser {
+    public static void main(String[] args) {
+        try {
+            //Obtain URL
+            URL dp = new URL("https://www.dianping.com");
+            //Download URL
+            //Get binary stream from URL by openStream()
+            //InputStream is = dp.openStream();
+            //模拟浏览器
+            HttpsURLConnection connection = (HttpsURLConnection) dp.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent","Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Mobile Safari/537.36");
+            InputStream is = connection.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is,"UTF-8"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("dp.html"));
+            String msg = null;
+            while ((msg=br.readLine())!=null){
+                System.out.println(msg);
+                writer.write(msg+"\n");
+            }
+            writer.flush();
+            writer.close();
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

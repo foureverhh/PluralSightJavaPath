9ef7011d07c7d0ccb1ffd9b212afd9d490efa216
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-11-12 15:49:53 +0100

    concurrent testing

--- PluralSight/JavaSE/ConcurrencyLearning/src/demo_all/dem02/TestTimer.java
+++ PluralSight/JavaSE/ConcurrencyLearning/src/demo_all/dem02/TestTimer.java
@@ -1,4 +1,34 @@
 package demo_all.dem02;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TestTimer {
+    public static void main(String[] args) throws InterruptedException {
+
+       Timer timer = new Timer(true);
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("I am from timer");
+                System.out.println("Timer "+Thread.currentThread().getName());
+            }
+        },1000);
+        System.out.println("Main "+Thread.currentThread().getName());
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                    System.out.println("Sleep 2 sec in"+Thread.currentThread().getName());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+        thread.join();
+        System.out.println("Main "+Thread.currentThread().getName());
+    }
 }
--- PluralSight/JavaSE/ConcurrencyLearning/src/demo_all/dem02/Worker.java
+++ PluralSight/JavaSE/ConcurrencyLearning/src/demo_all/dem02/Worker.java
@@ -4,14 +4,16 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Worker extends Thread{
+    private final Object key = new Object();
     private volatile boolean quittingTime = false;
 
     public void run(){
         while(!quittingTime){
             working();
-            System.out.println("Still working...");
+            System.out.println("Still working..."+Thread.currentThread().getName());
+
         }
-        System.out.println("Coffee is good !");
+        System.out.println("Coffee is good !"+Thread.currentThread().getName());
     }
 
     private void working(){
@@ -23,27 +25,50 @@ public class Worker extends Thread{
     }
 
     synchronized void quit() throws InterruptedException {
-        quittingTime = true;
-        join();
+        System.out.println("Quit "+Thread.currentThread().getName());
+        System.out.println("Quit outside: "+Thread.holdsLock(this));
+        System.out.println("Quit outside: "+Thread.holdsLock(key));
+        synchronized (key) {
+            quittingTime = true;
+            System.out.println("Calling join");
+            System.out.println("Quit inside "+Thread.currentThread().getName());
+            System.out.println("Quit inside: "+Thread.holdsLock(this));
+            System.out.println("Quit inside: "+Thread.holdsLock(key));
+            join();//join would call wait(0) and lost its key
+            System.out.println("Back from join");
+        }
     }
 
-    synchronized void keepWorking(){
-        quittingTime = false;
+    synchronized void keepWorking()  {
+        System.out.println("kod is here");
+        System.out.println("keepWorking ends here!!! " + Thread.currentThread().getName());
+        System.out.println("keep working outside: "+Thread.holdsLock(this));
+        System.out.println("keep working outside: "+Thread.holdsLock(key));
+        synchronized (key) {
+            System.out.println("keep working inside: "+Thread.holdsLock(this));
+            System.out.println("keep working inside: "+Thread.holdsLock(key));
+            quittingTime = false;
+            System.out.println("!!!!Keep working...");
+
+        }
+        System.out.println("!!!kod is also here");
     }
 
     public static void main(String[] args) throws InterruptedException {
-        final Worker worker = new Worker();
-        worker.start();
+        final Worker w = new Worker();
+        w.start();
 
         Timer t = new Timer(true);
         t.schedule(new TimerTask() {
             @Override
             public void run() {
-                worker.keepWorking();
+                w.keepWorking();
+                System.out.println("!!!Timer "+Thread.currentThread().getName());
             }
         },500);
 
         Thread.sleep(400);
-        worker.quit();
+        System.out.println("main "+Thread.currentThread().getName());
+        w.quit();
     }
 }

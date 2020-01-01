4688718d314a320481d38c0e9387ee401081950b
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2020-01-02 00:05:51 +0100

    tested Stream.Builder<T> xx = Stream.builer(), IntStream as string.chars(),Pattern.compile(reg).splitAsStream(str) Files.lines(Paths.get(fileName))

--- PluralSight/JavaSE/Java8LambdaExpression/src/Streams_part5/MainBuildingStreams.java
+++ PluralSight/JavaSE/Java8LambdaExpression/src/Streams_part5/MainBuildingStreams.java
@@ -1,8 +1,13 @@
 package Streams_part5;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -24,5 +29,25 @@ public class MainBuildingStreams {
         IntStream streamOfInt = ThreadLocalRandom.current().ints();
         streamOfInt.limit(5).forEach(System.out::println);
 
+        Stream.Builder<Integer> streamBuilder = Stream.builder();
+        streamBuilder.add(1).add(2).add(3);
+        streamBuilder.accept(4);
+        Stream myIntStream = streamBuilder.build();
+        myIntStream.forEach(System.out::println);
+
+        IntStream charStream = "hello".chars();
+        charStream.mapToObj(c -> (char)c).forEach(System.out::println);
+
+        String target = "Test* Java&Letter!!";
+        //SplitAsStream split the string around matches and save as Stream<String>
+        Stream<String> words = Pattern.compile("[^\\p{javaLetter}]").splitAsStream(target);
+        words.forEach(System.out::println);
+
+        try {
+            Stream<String> file = Files.lines(Paths.get("test.txt"));
+            file.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
